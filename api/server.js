6550f495e22be0da67371114aee28635b59c3b53
adastraz <tylergosox@gmail.com>
2020-06-20 18:20:56 -0400

    inital for expat

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const server = express()
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.json({ message: 'Server is up and running'})
+})
+
+module.exports = server
---
+++ data/migrations/20200620181559_blog.js
@@ -0,0 +1,55 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('users', tbl => {
+            tbl.increments()
+            tbl.string('username', 32).unique().notNullable()
+            tbl.string('password', 64).notNullable()
+            tbl.string('description', 128)
+            tbl.string('name', 64).notNullable()
+            tbl.string('company', 128)
+        })
+
+        .createTable('stories', tbl => {
+            tbl.increments()
+            tbl.string('def_image').notNullable()
+            tbl.string('2def_img').notNullable()
+            tbl.string('3def_img').notNullable()
+            tbl.string('story_desc').notNullable()
+            tbl.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE')
+            tbl.integer('story_likes').defaultTo(0)
+        })
+
+        .createTable('images', tbl => {
+            tbl.increments()
+            tbl.integer('story_id')
+                .unsigned()
+                .references('id')
+                .inTable('stories')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE')
+            tbl.string('img_url').notNullable()
+            tbl.integer('img_likes').defaultTo(0)
+        })
+
+        .createTable('comments', tbl => {
+            tbl.increments()
+            tbl.integer('img_id')
+                .unsigned()
+                .references('id')
+                .inTable('images')
+                .onDelete('CASCADE')
+            tbl.string('comment', 255).notNullable()
+        })
+}
+
+
+exports.down = function(knex) {
+  
+}
